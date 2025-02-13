85a510100d07798412de006493550d2bb2564927
SadiqR044 <sadiqr044@gmail.com>
2025-02-13 14:27:07 +0500

    Cleaned project

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)  # Product name (Text)
+    price = models.DecimalField(max_digits=10, decimal_places=2)  # Product price (Decimal)
+    description = models.TextField()  # Product description (Large text)
+    created_at = models.DateTimeField(auto_now_add=True)  # Auto timestamp
+
+    def __str__(self):
+        return self.name  # This makes the name appear in the admin panel
